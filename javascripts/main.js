3308b4ae60e78b0a78fdcf5e35fadc5c3a01164a
Bruno Daniel <bruno@vault7.com>
2014-05-02 20:03:55 -0300

    Auto update download button with the GitHub API

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,16 @@
-console.log('This would be the main JS file.');
+$(function() {
+
+	var releasesURL = "https://api.github.com/repos/rev087/ng-inspector/releases?callback=foo";
+	$.ajax(releasesURL, {
+		dataType: "jsonp",
+		success: function(res) {
+			var latest = res.data[0],
+				downloadURL = latest.assets[0].url,
+				version = latest.name,
+				dlButton = $('<li><a href="'+downloadURL+'">Download <strong>'+version+'</strong></a></li>');
+			$('.dl-list').prepend(dlButton);
+		}
+	});
+
+
+});
