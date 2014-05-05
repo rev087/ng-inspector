1b546faa56e0866d7b16bdd3a1b29a7cf6e3fbe8
Bruno Daniel <bruno@vault7.com>
2014-05-04 23:52:33 -0300

    Fix download button

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,10 +4,12 @@ $(function() {
 	$.ajax(releasesURL, {
 		dataType: "jsonp",
 		success: function(res) {
+			console.log(res.data);
 			var latest = res.data[0],
 				downloadURL = latest.assets[0].url,
 				version = latest.name,
-				dlButton = $('<li><a href="'+downloadURL+'">Download <strong>'+version+'</strong></a></li>');
+				dlButton = $('<li><a href="ng-inspector.safariextz">Download <strong>'+version+'</strong></a></li>');
+				// dlButton = $('<li><a href="'+downloadURL+'">Download <strong>'+version+'</strong></a></li>');
 			$('.dl-list').prepend(dlButton);
 		}
 	});
