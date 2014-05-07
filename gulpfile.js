cb435aa287b0a888c6e2f737a54376fa9eb6a265
Bruno Daniel <bruno@vault7.com>
2014-05-07 07:25:26 -0300

    Add semver bumping gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var wrap = require('gulp-wrap');
+var semver = require('semver');
+var fs = require('fs');
+var colors = require('colors');
+var plist = require('plist');
+var spawn = require('child_process').spawn;
+
 
 gulp.task('build', function() {
 
@@ -18,5 +24,68 @@ gulp.task('build', function() {
 	// http://developer.streak.com/2013/01/how-to-build-safari-extension-using.html
 
 });
+
+function run(cmd, args, callback) {
+	var child = spawn(cmd, args);
+	var buffer = '';
+	child.stdout.on('data', function (data) { buffer = buffer + data; });
+	child.stderr.on('data', function (data) { console.error(data.toString()); });
+	child.on('close', function(code) {
+		if (code !== 0) throw cmd + ' process exited with code ' + code;
+		else callback.apply(this, [buffer]);
+	});
+}
+
+function bump(release) {
+
+	// Check if the stage is clean
+	run('git', ['diff', '--staged'], function(res) {
+		if (res.length > 0) {
+			return console.error('\n ' + ' BUMP ERROR '.redBG.bold.black +
+				' Cannot update manifests with a dirty Git stage. \n'.red);
+		}
+
+		// Bump the version in package.json
+		var pkg = require('./package.json');
+		var old = pkg.version;
+		pkg.version = 'v' + semver.inc(pkg.version, release);
+		var jsonStr = JSON.stringify(pkg, null, 2);
+		fs.writeFileSync('package.json', jsonStr);
+
+		// Bump the version in Info.plist
+		var info = plist.parseFileSync('ng-inspector.safariextension/Info.plist');
+		info.CFBundleShortVersionString = pkg.version;
+		info.CFBundleVersion = pkg.version;
+		var plistStr = plist.build(info).toString();
+		fs.writeFileSync('ng-inspector.safariextension/Info.plist', plistStr);
+
+		// Git add
+		run('git', ['add', 'package.json', 'ng-inspector.safariextension/Info.plist'], function() {
+
+			// Git commit
+			var commitMsg = 'Prepare for ' + pkg.version;
+			run('git', ['commit', '-m', commitMsg], function() {
+
+				// Git tag
+				run('git', ['tag', pkg.version], function() {
+
+					// Print a virtual congratulatory pat on the back
+					var msg = ('\n "' + pkg.name + '"').cyan.bold +
+					' bumped from ' + old.green.underline + ' to ' +
+					pkg.version.magenta.underline + '\n';
+					console.log(msg);
+
+				}); // tag
+
+			}); // commit
+
+		}); // add
+
+	}); // check stage
+}
+
+gulp.task('bump:major', function() { bump('major'); });
+gulp.task('bump:minor', function() { bump('minor'); });
+gulp.task('bump:patch', function() { bump('patch'); });
 
 gulp.task('default', ['build']);
