efaff38b5eb1b1b27cf6d9edd0bdd024467fa400
Bruno Daniel <bruno@vault7.com>
2014-05-21 11:16:38 -0300

    Add Chrome Web Store link

--- javascripts/main.js
+++ javascripts/main.js
@@ -9,7 +9,7 @@ $(function() {
 				downloadURL = latest.assets[0].url,
 				version = latest.name,
 				safariButton = $('<li><a href="ng-inspector.safariextz">'+version+'<strong>for Safari</strong></a></li>'),
-				chromeButton = $('<li><a href="ng-inspector.chrome.crx">'+version+'<strong>for Chrome</strong></a></li>');
+				chromeButton = $('<li><a href="https://chrome.google.com/webstore/detail/ng-inspector/aadgmnobpdmgmigaicncghmmoeflnamj">'+version+'<strong>for Chrome</strong></a></li>');
 			$('.dl-list').prepend(safariButton);
 			$('.dl-list').prepend(chromeButton);
 		}
