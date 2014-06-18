7aaf519455dae69d9d87c80ae17f49e274d4cb03
Bruno Daniel <bruno@vault7.com>
2014-06-18 15:59:57 -0300

    Remove images from the test build CSS

--- gulpfile.js
+++ gulpfile.js
@@ -109,9 +109,10 @@ gulp.task('build:css', function() {
 	return gulp.src(['src/less/stylesheet.less'])
 		.pipe(less())
 		.pipe(gulp.dest('ng-inspector.safariextension/'))
-		.pipe(gulp.dest('test/e2e/fixtures/'))
-		.pipe(replace(/url\(/g, 'url(chrome-extension://__MSG_@@extension_id__/'))
-		.pipe(gulp.dest('ng-inspector.chrome/'));
+		.pipe(replace(/url\(/g, 'url(chrome-extension://__MSG_@@extension_id__/')) // Add path prefix for Chrome
+		.pipe(gulp.dest('ng-inspector.chrome/'))
+		.pipe(replace(/(\s*)(.+url\(.+)/g, '$1/* $2 */$1background-image: none !important;')) // Remove images from the test build
+		.pipe(gulp.dest('test/e2e/fixtures/'));
 });
 
 // Here would be a good place to build the Safari archive. But it requires a
