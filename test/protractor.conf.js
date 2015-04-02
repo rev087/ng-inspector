3003685020f4a81aeb363297e64aba430282fd2d
Bruno Daniel <bruno@vault7.com>
2015-04-02 07:36:07 -0300

    Add comment pointing to appropriate version of protractor API

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -1,4 +1,5 @@
-// An example configuration file.
+// Docs for protractor 0.24:
+// https://github.com/angular/protractor/blob/8582b195ed0f4d48a0d9513017b21e99f8feb2fe/docs/api.md
 exports.config = {
   sauceUser: process.env.SAUCE_USERNAME,
   sauceKey: process.env.SAUCE_ACCESS_KEY,
