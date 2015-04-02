5022bfda0e224ef5199108a0b2b3f9355dfc888e
Bruno Daniel <bruno@vault7.com>
2015-04-02 07:36:38 -0300

    Add test for PR #71

---
+++ test/e2e/specs/empty-directive.js
@@ -0,0 +1,20 @@
+browser.ignoreSynchronization = true;
+var $$ = function(query) { return element.all(by.css(query)); }
+var $ = function(query) { return element(by.css(query)); }
+
+describe('empty directive', function() {
+
+	var _warn, _warnMessage;
+
+  beforeEach(function () {
+		browser.get('/empty-directive.html');
+		element(by.id('ngInspectorToggle')).click();
+		browser.sleep(250);
+  });
+
+	it('should warn about empty directive declarations', function() {
+		expect($('#warning').getText())
+			.toBe('Invalid directive found. Make sure all registered directives return a "Directive Definition Object"');
+	});
+
+});
