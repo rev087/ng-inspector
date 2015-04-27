7e32b415e19ab28141eec135ff397df121ca1b59
Andrew Levine <andrewl@onprem.com>
2015-04-26 22:15:14 -0500

    Make empty-directive test much more explicit

--- test/e2e/specs/empty-directive.js
+++ test/e2e/specs/empty-directive.js
@@ -7,14 +7,17 @@ describe('empty directive', function() {
 		element(by.id('ngInspectorToggle')).click();
 	});
 
-	it('should not throw exception when directive with DDO is used', function() {
+	it('should not throw exception when directive with empty DDO is used', function() {
 		browser.manage().logs().get('browser').then(function (browserLog) {
 			var severeWarnings = browserLog.filter(function(log) {
 				// Not concerned about info or warnings
 				return log.level.name === 'SEVERE';
 			});
 
-			expect(severeWarnings.length).toBe(0);
+			severeWarnings.forEach(function(warning) {
+				// https://github.com/rev087/ng-inspector/issues/39
+				expect(warning.message).toNotMatch(/Cannot read property 'restrict' of undefined/i);
+			});
 		});
 	});
 
